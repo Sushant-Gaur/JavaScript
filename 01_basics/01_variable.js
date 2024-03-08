012f71e2aa005eee343cfffc8d4b650c9c06e177
Sushant-Gaur <137107997+Sushant-Gaur@users.noreply.github.com>
2024-03-08 19:24:27 +0000

    container config done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,13 @@
+const accountId =144553
+let accountEmail="sushant@gmai.com"
+var accountPassword="1234"
+accountCity="kanpur"
+
+accountEmail="h1@gmail.com"
+accountPassword="6565765"
+accountCity="Mohali"
+
+//accountId =2 // not allowed
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
