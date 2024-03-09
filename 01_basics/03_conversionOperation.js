5c64cdb464547d0c00fc4c0495ef448274d51986
Sushant-Gaur <137107997+Sushant-Gaur@users.noreply.github.com>
2024-03-09 09:04:49 +0000

    conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -13,8 +13,57 @@ console.log(valueInNumber);
 console.log(typeof valueInNumber1);
 console.log(valueInNumber1); */
 
-let isLoggedIn = 1
+/* let isLoggedIn = 1
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 console.log(typeof isLoggedIn)
-console.log(booleanIsLoggedIn)
+console.log(booleanIsLoggedIn) */
+
+/* let x= 1;
+let x1 = String(x);
+console.log(x1);
+console.log( typeof x1); */
+
+/* Operations */
+
+/* let value =3
+let negvalue = -value;
+
+//console.log(negvalue);
+
+console.log(2+2);
+
+console.log(2-2);
+console.log(2**2); // 2 power 2
+console.log(5%2); */
+
+/* let str1="hello";
+let str2 ="Sushant";
+
+let str3= str1+str2;
+console.log(str3); */
+
+/* console.log ("1" + 2);
+console.log (1 + "2");
+console.log ("1" + 2 + 2);
+console.log (1 + 2 + "2"); */
+
+/* console.log(true);
+console.log(+true);
+console.log(+""); */
+
+/* let gameCounter= 100;
+gameCounter++;
+console.log(gameCounter) */
+
+/* let gameCounter= 100;
+++gameCounter;
+console.log(gameCounter) */
+
+/* let x=5;
+let y=x++;
+console.log(x,y);  post increment */
+
+/* let y= 5;
+let x= ++y;
+console.log (x,y) */
