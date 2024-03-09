fb2557377ed55c76fccda3cd87851323ff951ac7
Sushant-Gaur <137107997+Sushant-Gaur@users.noreply.github.com>
2024-03-09 19:58:35 +0000

    datatype

---
+++ 01_basics/datatype.js
@@ -0,0 +1,22 @@
+/* stack*/
+/* let myYouubeName = "sushant_vlog";
+
+let anotherName = myYouubeName
+
+anotherName = "gaur_vlog"
+
+console.log(myYouubeName);
+console.log(anotherName); */
+
+/*  heap */
+let userOne={
+    email: "sushant@123",
+    upi : "user@ybl"
+}
+let userTwo =userOne
+
+userTwo.email="sushant@.com";
+
+console.log(userOne.email)
+console.log(userTwo.email)
+
