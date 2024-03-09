05f76a61f9dd0f9601172235e7a9df79ccafac67
Sushant-Gaur <137107997+Sushant-Gaur@users.noreply.github.com>
2024-03-09 08:04:40 +0000

    conversion of datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,20 @@
+/* let score = "33abc";
+let score1 = null ;
+
+console.log(typeof score);
+console.log(typeof (score1));
+
+
+let valueInNumber= Number(score);
+let valueInNumber1= Number(score1);
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+console.log(typeof valueInNumber1);
+console.log(valueInNumber1); */
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(typeof isLoggedIn)
+console.log(booleanIsLoggedIn)
