7bf0039898a021fad7bec40832a4462bb2256450
Sushant-Gaur <137107997+Sushant-Gaur@users.noreply.github.com>
2024-03-12 19:03:10 +0000

    string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,24 @@
+/* const nam='sushant';
+
+const repoCount=50
+//console.log(nam + repoCount);
+
+//console.log(`hello my name is ${nam} and my repo is ${repoCount}`);
+console.log(`${nam}`); */
+
+const gameName = new String ('sushant');
+/* console.log(gameName);
+console.log(gameName[0]);
+console.log(gameName.length);
+console.log(gameName.toUpperCase()); 
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));*/
+const newString = gameName.substring(0,4);
+console.log(newString);
+
+const anotherString = gameName.slice(-2,7);
+console.log(anotherString);
+
+
+
+
---
+++ 01_basics/let.js
@@ -0,0 +1,17 @@
+/* let x= 20;{
+    x=10;
+    console.log(x);
+} */
+
+const y= 20;{
+      y=10;
+    console.log(x);
+}
+
+
+
+
+
+
+
+
